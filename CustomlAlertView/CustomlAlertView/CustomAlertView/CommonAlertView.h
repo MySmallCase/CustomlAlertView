567baeb499629baad2d7344d5969a5ec757fa7d1
向云飞 <978771447@qq.com>
2016-02-14 17:34:58 +0800

    弹窗模拟各种情况完毕

---
+++ CustomlAlertView/CustomlAlertView/CustomAlertView/CommonAlertView.h
@@ -0,0 +1,51 @@
+//
+//  CommonAlertView.h
+//  CustomlAlertView
+//
+//  Created by MyMac on 16/2/14.
+//  Copyright © 2016年 MyMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@protocol CommonAlertViewDelegate
+
+- (void)customIOS7dialogButtonTouchUpInside:(id)alertView clickedButtonAtIndex:(NSInteger)buttonIndex;
+
+@end
+
+@interface CommonAlertView : UIView<CommonAlertViewDelegate>
+
+
+@property (nonatomic,strong) UIView *parentView;    /**< 父视图*/
+@property (nonatomic,strong) UIView *dialogView;    /**< 弹窗视图*/
+@property (nonatomic,strong) UIView *containerView;  /**< 弹窗中的容器视图 (在这里添加UI)*/
+@property (nonatomic,strong) UIView *buttonView;    /**< 添加底部按钮的视图*/
+
+
+@property (nonatomic,strong) NSArray *buttonTitles; /**< 按钮文字*/
+@property (nonatomic,strong) NSArray *buttonBgColors; /**< 按钮背景颜色*/
+
+@property (nonatomic,strong) UIColor *buttonBgViewColor; /**< 按钮底部背景色*/
+@property (nonatomic,strong) UIFont *buttonTextFont; /**< 按钮文字字体*/
+@property (nonatomic,strong) UIColor *buttonTextColor; /**< 按钮文字颜色*/
+@property (nonatomic,assign) BOOL clickedBgCloseAlertView; /**< 点击后面背景关闭弹窗  默认关闭*/
+
+@property (nonatomic,weak) id<CommonAlertViewDelegate> delegate; /**< 代理*/
+
+@property (copy) void (^onButtonTouchUpInside)(CommonAlertView *alertView, int buttonIndex) ;
+
+- (instancetype)init;
+
+- (instancetype)initWithParentView:(UIView *)parentView;
+
+- (void)show;
+- (void)close;
+
+//- (void)dialogButtonTouchUpInside:(id)sender;
+- (void)setOnButtonTouchUpInside:(void (^)(CommonAlertView *alertView, int buttonIndex))onButtonTouchUpInside;
+
+//- (void)deviceOrientationDidChange: (NSNotification *)notification;
+//- (void)dealloc;
+
+@end
--- CustomlAlertView/CustomlAlertView/ViewController.m
+++ CustomlAlertView/CustomlAlertView/ViewController.m
@@ -7,8 +7,9 @@
 //
 
 #import "ViewController.h"
+#import "CommonAlertView.h"
 
-@interface ViewController ()
+@interface ViewController ()<CommonAlertViewDelegate>
 
 @end
 
@@ -16,12 +17,50 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    UIButton *launchDialog = [UIButton buttonWithType:UIButtonTypeCustom];
+    [launchDialog setFrame:CGRectMake(10, 30, self.view.bounds.size.width-20, 50)];
+    [launchDialog addTarget:self action:@selector(launchDialog:) forControlEvents:UIControlEventTouchDown];
+    [launchDialog setTitle:@"Launch Dialog" forState:UIControlStateNormal];
+    [launchDialog setBackgroundColor:[UIColor whiteColor]];
+    [launchDialog setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+    [launchDialog.layer setBorderWidth:0];
+    [launchDialog.layer setCornerRadius:5];
+    [self.view addSubview:launchDialog];
+    
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)launchDialog:(id)sender {
+    CommonAlertView *alertView = [[CommonAlertView alloc] init];
+    
+    alertView.containerView = [self createDemoView];
+//    alertView.buttonTitles = @[@"残忍拒绝", @"任性投票", @"123"];
+//    alertView.buttonBgColors = @[[UIColor redColor],[UIColor blueColor],[UIColor greenColor]];
+    alertView.buttonTextFont = [UIFont boldSystemFontOfSize:11.0f];
+    alertView.buttonTextColor = [UIColor redColor];
+    alertView.delegate = self;
+    alertView.clickedBgCloseAlertView = YES;
+    [alertView show];
 }
+
+- (void)customIOS7dialogButtonTouchUpInside: (CommonAlertView *)alertView clickedButtonAtIndex: (NSInteger)buttonIndex {
+    if (buttonIndex == 0) {
+        [alertView close];
+    }else {
+        NSLog(@"任性投票");
+    }
+}
+
+- (UIView *)createDemoView
+{
+    UIView *demoView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 290, 200)];
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 290, 200)];
+    imageView.backgroundColor = [UIColor grayColor];
+    [demoView addSubview:imageView];
+    
+    return demoView;
+}
+
 
 @end
